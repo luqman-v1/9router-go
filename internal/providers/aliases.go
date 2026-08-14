b3625e77149b7b3518d5d4edb729594acb4fd94d
Luqmannul Hakim <luqmannul.hakim@stockbit.com>
2026-08-14 22:32:37 +0700

    revert: remove agy from ProviderAliasMap as it is a user-defined combo name

--- internal/providers/aliases.go
+++ internal/providers/aliases.go
@@ -4,7 +4,6 @@ package providers
 var ProviderAliasMap = map[string]string{
 	"aai":            "assemblyai",
 	"ag":             "antigravity",
-	"agy":            "antigravity",
 	"ali":            "alicode",
 	"ali-tp":         "alitp-intl",
 	"alii":           "alicode-intl",
